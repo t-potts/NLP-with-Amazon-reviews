ead25fc844d27f151dd8cb816c6ed966311215da
t-potts <tylerjacobpotts@gmail.com>
2019-07-02 17:18:24 -0500

    working script

--- spark_script.py
+++ spark_script.py
@@ -21,22 +21,26 @@ import time
 
 start_time = time.time()
 
-progress = open('progress.txt', 'w')
+
 
 sc = SparkContext()
 spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
 
 data = sc.textFile('s3://amazon-reviews-pds/tsv/amazon_reviews_us_Video_Games_v1_00.tsv.gz')
 df = spark.read.csv(data, sep="\t", header=True, inferSchema=True)
-progress.write('---Loaded data into dataframe---\n')
 
-stop_words = set(stopwords.words('english'))
-stop_words.add('')
+
+
 
 # removes punctuation, adds column 'body_list' which is a list of words, and selects only the star_rating and body_list columns
 stop_words = set(stopwords.words('english'))
 for word in ['', 1, 2, 3, 4, 5]:
     stop_words.add(word)
+stop_words_broadcast = sc.broadcast(stop_words)
+
+stemmer = sc.broadcast(PorterStemmer())
+strip_chars = ".?,!;:\"'()" 
+rgx = sc.broadcast(re.compile('[%s]' % strip_chars) )
 
 def process_str(row):
     """Input: String
@@ -44,27 +48,23 @@ def process_str(row):
     
     Removes punctuation using functions from the strings library and stems using NLK. 
     ."""
+    
+    body_list = []
     try:
-        stemmer = PorterStemmer()
-        strip_chars = ".?,!;:\"'()" 
-        rgx = re.compile('[%s]' % strip_chars) 
-
-        body_list = []
         for word in row.lower().split(): 
-            word = rgx.sub('', word) 
-            if word not in stop_words: 
-                body_list.append(stemmer.stem(word))
-        
+            word = rgx.value.sub('', word) 
+            if word not in stop_words_broadcast.value: 
+                body_list.append(stemmer.value.stem(word))
         return body_list
     except Exception as e:
-        progress.write(e)
-        return []
+        print(e)
+        return ['']
 
 process = udf(process_str, ArrayType(StringType()))
-print("line 64")
+
 df_new = df.withColumn('body_list', process(df['review_body']))\
-        .select('star_rating', 'body_list').limit(5000)
-print("line 67")
+        .select('star_rating', 'body_list')
+print("-"*30, '\nFinished creating new_df\n', "-"*30)
 cv = CountVectorizer(inputCol="body_list", outputCol="features", minDF=0.001)
 idf = IDF(inputCol='features', outputCol = 'idf_features')
 rf = RandomForestClassifier(labelCol='star_rating', featuresCol='idf_features', seed=100)
@@ -72,17 +72,17 @@ rf = RandomForestClassifier(labelCol='star_rating', featuresCol='idf_features', 
 # Stage one in pipeline the words are separated into count vectors
 stage1_fit = cv.fit(df_new)
 stage1_transform = stage1_fit.transform(df_new)
-progress.write("---Finished Stage 1---\n")
+print("-"*30, '\nFinished stage 1\n', "-"*30)
 
 # Stage two in pipeline for tf-IDF
 stage2_fit = idf.fit(stage1_transform)
 stage2_transform = stage2_fit.transform(stage1_transform)
-progress.write("---Finished Stage 2---\n")
+print("-"*30, '\nFinished stage 2\n', "-"*30)
 
 # Stage three in pipeline for random forest
 stage3_fit = rf.fit(stage2_transform)
 stage3_transform = stage3_fit.transform(stage2_transform)
-progress.write("---Finished Stage 3---\n")
+print("-"*30, '\nFinished stage 3\n', "-"*30)
 
 def print_top_features(model, n=10):
     """
@@ -96,7 +96,4 @@ print_top_features(stage3_fit)
 
 pd = stage3_transform.select('star_rating', 'idf_features').toPandas()
 pd.to_pickle('idf_pandas')
-
-time_spent = time.time() - start_time
-progress.write('File took {:.2f} seconds to run'.format(time_spent))
-progress.close()
+print("-"*30, '\nsaved pandas to a pickle file\n', "-"*30)
