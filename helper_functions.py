3597c181e449839d4f02187f91b0d9da811eaa27
Tyler-Speider <tylerjacobpotts@gmail.com>
2019-07-26 16:09:59 -0500

    bug with processed data

--- helper_functions.py
+++ helper_functions.py
@@ -54,7 +54,9 @@ def import_data(json_path):
 
     vocab_size_saved = False
     with open(json_path, 'r') as f:
-        for line in f:
+        for i, line in enumerate(f):
+            if i == 50000: break
+
             #creates separate lists for sparse vec data, and star review
             count_vec_list.append(json.loads(line)['count_vec'])
             star_rating_list.append(json.loads(line)['star_rating'])
@@ -78,147 +80,21 @@ def import_data(json_path):
     
     return dense_matrix, np.array(star_rating_list)
 
+def count_stars(json_path):
+    count = 0
+    with open(json_path, 'r') as f:
+        for line in f:
 
-def batch_generator(file_dir, batch_size, samples_per_epoch):
-    #creates list of filename strings from jsons directory
-    dir_list = listdir('jsons')
-    
-    #Determines number of batches to produce
-    number_of_batches = samples_per_epoch/batch_size
-    counter=0
-    file_counter = 0
-    
-    #Creates random shuffle index
-    file_shuffle_index = np.arange(len(dir_list))
-    np.random.shuffle(file_shuffle_index)
-    
-    #Uses first random shuffle index to select a file
-    X, y = import_data('jsons/' + dir_list[file_shuffle_index[file_counter]])
-    
-    #Creates shuffle index arrays to randomize the data rows as well as the files used
-    data_shuffle_index = np.arange(np.shape(X)[0])
-    np.random.shuffle(data_shuffle_index)
-    
-    while 1:
-        #Randomizes selection of data rows
-        index_batch = data_shuffle_index[batch_size*counter:batch_size*(counter+1)]
-        X = X[index_batch, :]
-        y = y[index_batch]
-        
-        #Saves the smallest count of good/bad reviews 
-        smallest_good_or_bad_count = min([y[y == 0].shape[0], y[y == 0].shape[0]])
-        
-        #Creates an array with a 50/50 split of good/bad reviews
-        X_batch_good = X[y == 1][0:smallest_good_or_bad_count, :]
-        X_batch_bad = X[y == 0][0:smallest_good_or_bad_count, :]
-        y_batch_good = y[y == 1][0:smallest_good_or_bad_count]
-        y_batch_bad = y[y == 0][0:smallest_good_or_bad_count]
-        
-        #Stacks the arrays
-        X_batch = np.vstack((X_batch_good, X_batch_bad))
-        y_batch = np.vstack((y_batch_good.reshape(-1, 1), y_batch_bad.reshape(-1,1)))
-        
-        #Randomizes the reviews so there isn't a block of good/bad reviews
-        batch_shuffle_index = np.arange(X_batch.shape[0])
-        np.random.shuffle(batch_shuffle_index)
-        X_batch = X_batch[batch_shuffle_index, :]
-        y_batch = y_batch[batch_shuffle_index]
-        
-        counter += 1
-        
-        """if autoencoder_layer:
-            yield X_batch, X_batch
-        else:"""
-        yield X_batch, y_batch
-        
-        
-        if (counter >= number_of_batches):
-            counter=0
-            file_counter +=1
-            
-            if file_counter == len(dir_list):
-                np.random.shuffle(file_shuffle_index)
-                file_count = 0
-                
-            #loads next file and resets shuffle index
-            X, y = import_data('jsons/' + dir_list[file_shuffle_index[file_counter]])
-            data_shuffle_index = np.arange(np.shape(X)[0])
-            np.random.shuffle(data_shuffle_index)
+            if json.loads(line)['star_rating'] == 0:
+                count += 1
+    return count
 
-def batch_generator_old(file_dir, batch_size, samples_per_epoch):
-    #creates list of filename strings from jsons directory
-    dir_list = listdir('jsons')
-    
-    #Determines number of batches to produce
-    number_of_batches = samples_per_epoch/batch_size
-    counter=0
-    file_counter = 0
-    
-    #Creates random shuffle index
-    file_shuffle_index = np.arange(len(dir_list))
-    np.random.shuffle(file_shuffle_index)
-    
-    #Uses first random shuffle index to select a file
-    X, y = import_data('jsons/' + dir_list[file_shuffle_index[file_counter]])
-    
-    #Creates shuffle index arrays to randomize the data rows as well as the files used
-    data_shuffle_index = np.arange(np.shape(X)[0])
-    np.random.shuffle(data_shuffle_index)
-    
-    while 1:
-        #Randomizes selection of data rows
-        index_batch = data_shuffle_index[batch_size*counter:batch_size*(counter+1)]
-        X = X[index_batch, :]
-        y = y[index_batch]
-        
-        #Saves the smallest count of good/bad reviews 
-        smallest_good_or_bad_count = min([y[y == 0].shape[0], y[y == 0].shape[0]])
-        
-        #Creates an array with a 50/50 split of good/bad reviews
-        X_batch_good = X[y == 1][0:smallest_good_or_bad_count, :]
-        X_batch_bad = X[y == 0][0:smallest_good_or_bad_count, :]
-        y_batch_good = y[y == 1][0:smallest_good_or_bad_count]
-        y_batch_bad = y[y == 0][0:smallest_good_or_bad_count]
-        
-        #Stacks the arrays
-        X_batch = np.vstack((X_batch_good, X_batch_bad))
-        y_batch = np.vstack((y_batch_good.reshape(-1, 1), y_batch_bad.reshape(-1,1)))
-        
-        #Randomizes the reviews so there isn't a block of good/bad reviews
-        batch_shuffle_index = np.arange(X_batch.shape[0])
-        np.random.shuffle(batch_shuffle_index)
-        X_batch = X_batch[batch_shuffle_index, :]
-        y_batch = y_batch[batch_shuffle_index]
-        
-        counter += 1
-        
-        y_out = np.zeros([y_batch.shape[0], 2])
-        y_out[:, 0] = y_batch.reshape(-1,)
-        y_out[:, 1] = y_out[:, 1] + 1
-        y_out[:, 1] = y_out[:, 1] - y_out[:, 0]
-       
-        yield X_batch, y_out
-        
-        if (counter >= number_of_batches):
-            counter=0
-            file_counter +=1
-            
-            if file_counter == len(dir_list):
-                np.random.shuffle(file_shuffle_index)
-                file_count = 0
-                
-            #loads next file and resets shuffle index
-            X, y = import_data('jsons/' + dir_list[file_shuffle_index[file_counter]])
-            data_shuffle_index = np.arange(np.shape(X)[0])
-            np.random.shuffle(data_shuffle_index)
-
-
-def import_data(json_directory, num_rows):
+def batch_generator(json_directory, num_rows):
     """
-    Parameter: String
+    Parameter: String, num_rows
     Output: 2 Numpy arrays
     
-    Reads json from path 'json_path' that has multiple rows of dense matrix and a star rating.
+    Reads jsons in path 'json_directory' that has multiple rows of dense matrix and a star rating.
     Processes the file, creates a dense matrix.
     Returns the dense matrix and star rating matrix
     """
@@ -233,7 +109,7 @@ def import_data(json_directory, num_rows):
     while 1:
         
         vocab_size_saved = False
-        with open(json_paths[path_shuffle[file_count]], 'r') as f:
+        with open(json_directory + '/' + json_paths[path_shuffle[file_count]], 'r') as f:
             for line in f:
                 #creates separate lists for sparse vec data, and star review
                 if json.loads(line)['star_rating'] == 1:
@@ -257,29 +133,49 @@ def import_data(json_directory, num_rows):
                     num_positive, num_negative = 0, 0
                     #creates an empty matrix of the size to fit the read in data
                     #adds an extra column to hold positive/negative value
-                    dense_matrix = np.zeros([num_rows, num_matrix_columns + 1])          
+                    dense_matrix = np.zeros((num_rows, num_matrix_columns + 1))
+
+                    #randomizes positive indexs
+                    positive_shuffle_idx = np.arange(len(count_vec_list_positive))
+                    np.random.shuffle(positive_shuffle_idx)
 
                     #loops through positive reviews to create dense matrix
-                    for i, row in enumerate(count_vec_list_positive):
+                    for i in range(len(count_vec_list_positive)):
+                        
+                        #Breaks loop if half the matrix is filled
+                        if i == num_rows / 2: break
+                        
+                        #selects a random row and gets the indices and values
+                        row = count_vec_list_positive[positive_shuffle_idx[i]]
                         indices = row['indices']
                         values = row['values']
-
+                        
                         #Replaces the indices of row i in the dense_matrix with the values
                         np.put(dense_matrix[i, :], indices, values)
 
                         #sets last column to indicate positive
                         dense_matrix[i, -1] = 1
 
+                    #randomizes negative indexs
+                    negative_shuffle_idx = np.arange(len(count_vec_list_positive))
+                    np.random.shuffle(negative_shuffle_idx)
+
                     #loops through negative reviews to create dense matrix
-                    for i, row in enumerate(count_vec_list_positive):
+                    for i in range(len(count_vec_list_negative)):
+                        #Breaks loop if the matrix is filled
+                        if i == num_rows / 2: break  
+                        
+                        #selects a random row and gets the indices and values
+                        row = count_vec_list_positive[negative_shuffle_idx[i]]
                         indices = row['indices']
                         values = row['values']
 
                         #Replaces the indices of row i in the dense_matrix with the values
-                        np.put(dense_matrix[i + (num_rows / 2), :], indices, values)
+                        row_idx = int(i + num_rows / 2)
+                        np.put(dense_matrix[row_idx, :], indices, values)
 
                         #sets last column to indicate negative
-                        dense_matrix[i + (num_rows / 2), -1] = 0
+                        dense_matrix[int(i + num_rows / 2), -1] = 0
                     
                     #Creates an array of randomized indexs with lengh equal to number of dense_matrix rows
                     shuffle_dense_indexs = np.arange(dense_matrix.shape[0])
