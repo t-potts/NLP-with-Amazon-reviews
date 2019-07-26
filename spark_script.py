469275640a246e1e267ed79859e7740b7c680764
Tyler-Speider <tylerjacobpotts@gmail.com>
2019-07-26 16:09:28 -0500

    debugging spark sript

--- spark_script.py
+++ spark_script.py
@@ -44,7 +44,10 @@ def main():
 
 	#Directory of reviews: s3://amazon-reviews-pds/tsv/
 	#The use of wildcards (*_us_*.gz) allows spark to load all but the non-english reviews
-	full_df = spark.read.csv('s3://amazon-reviews-pds/tsv/*_us_*.gz', sep="\t", header=True, inferSchema=True)
+	#full_df = spark.read.csv('s3://amazon-reviews-pds/tsv/*_us_*.gz', sep="\t", header=True, inferSchema=True)
+	
+	full_df = spark.read.csv('s3://amazon-reviews-pds/tsv/amazon_reviews_us_Video_v1_00.tsv.gz', sep="\t", header=True, inferSchema=True)
+
 	
 	#Uncomment the below line and comment the above to only process a subset of the data
 	#full_df = spark.read.csv('s3://amazon-reviews-pds/tsv/amazon_reviews_us_Video_DVD_v1_00.tsv.gz', sep="\t", header=True, inferSchema=True)
@@ -58,6 +61,8 @@ def main():
 	filtered_df = full_df.select('review_headline', 'review_body', 'star_rating')\
 	   .filter(full_df.star_rating != 3)\
 			.withColumn('star_rating', good_bad('star_rating'))
+
+	
 
 	#Concatinates the review_headline and review_body columns and renames the column 'text'
 	two_col_df = filtered_df.select(concat(col('review_headline'), lit(' '), col('review_body')).alias('text'), filtered_df.star_rating)
@@ -74,8 +79,8 @@ def main():
 
 	#Saves the vocabulary and processed dataframe to S3 in JSON format
 	vocab = spark.createDataFrame(cv_fit.vocabulary, schema=StringType())
-	vocab.coalesce(1).write.mode("overwrite").json('s3://dsi-amazon-neural/vocab_full')
-	output_df.write.mode("overwrite").json('s3://dsi-amazon-neural/jsonfiles_full')
+	vocab.coalesce(1).write.mode("overwrite").json('s3://dsi-amazon-neural/debug_vocab')
+	output_df.write.mode("overwrite").json('s3://dsi-amazon-neural/debug_data')
 
 def good_bad_filter(x):
 	"""
